fc701b099e664807dcc6ceb0e29d90b7db98e237
Sviatoslav <slavik2003>
2021-05-15 12:58:59 +0300

    Task

--- MathIntepreter/ConsoleApplication1/AST.cpp
+++ MathIntepreter/ConsoleApplication1/AST.cpp
@@ -294,7 +294,12 @@ void AST::StartImplementation () {
 
 void AST::Implementation (Node *node) {
 
+	//cout << node->data << endl;
+
 	for (int i = 0; i < node->childrens.size(); i++) {
+		//cout << i << endl;
+
+		Implementation (node->childrens[i]);
 
 		if (node->childrens[i]->data == ":=") {
 			assign (node->childrens[i]);
@@ -310,6 +315,12 @@ void AST::Implementation (Node *node) {
 		else if (node->childrens[i]->data == "return") {
 			cout << calculate (node->childrens[i]->childrens[0]) << endl;
 			//return;
+		} else if (node->childrens[i]->data == "while") {
+			if  (condiction (node->childrens[i]->childrens[0])) {
+				//cout << "+++++++" << endl;
+				Implementation (node->childrens[i]->childrens[1]);
+				i--;
+			}
 		}
 		
 	}
